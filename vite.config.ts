da21862b3007a2ec5b985ec9b634971b00b54155
Peter Rudolf <peterrudolf249@gmail.com>
2025-03-20 16:26:02 -0400

    updated yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+		react(),
+		istanbul({
+			exclude: ["node_modules", "dist", "test", "cypress"],
+			cypress: true,
+			requireEnv: false
+		}),
+	],
   test: {
     globals: true,
     environment: 'jsdom',
